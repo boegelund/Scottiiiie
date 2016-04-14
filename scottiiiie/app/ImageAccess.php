42295afcd82e4477e690fc7b3c122b761526f1ef
Eiler Poulsen <microhive@outlook.com>
2016-04-14 18:29:19 +0200

    Added soft deletion

--- scottiiiie/app/ImageAccess.php
+++ scottiiiie/app/ImageAccess.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ImageAccess extends Model
 {
+    use SoftDeletes;
+    
     protected $table = 'image_access';
     
     public function user()
@@ -17,4 +20,6 @@ class ImageAccess extends Model
     {
         return $this->belongsTo('App\Image');
     }
+    
+    protected $dates = ['deleted_at'];
 }
